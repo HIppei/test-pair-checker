2362a52c2e7a731a2d5835d1368269b52b8fdeb1
HIppei <ip.horiba@gmail.com>
2023-12-18 09:51:18 +0900

    Add testing, prettier, husky and lint-staged

---
+++ prettierrc.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: "lf",
+};
+
+export default config;
